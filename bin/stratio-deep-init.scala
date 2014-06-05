a2905155e64f9ea6955213d70483bf42f433591a
Luca Rosellini <luca@stratio.com>
2014-06-05 17:44:25 +0200

    renamed DeepJobConfigFactory to Cfg

--- bin/stratio-deep-init.scala
+++ bin/stratio-deep-init.scala
@@ -3,7 +3,7 @@ import com.stratio.deep.annotations.DeepField;
 import com.stratio.deep.entity.IDeepType;
 import org.apache.cassandra.db.marshal.Int32Type;
 import org.apache.cassandra.db.marshal.LongType;
-import com.stratio.deep.config._;
+import com.stratio.deep.config.{DeepJobConfigFactory=>Cfg, _};
 import com.stratio.deep.entity._;
 import com.stratio.deep.context._;
 import com.stratio.deep.rdd._
