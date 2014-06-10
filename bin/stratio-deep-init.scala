b7865083cde2a364d3720831f683678048393896
Luca Rosellini <luca@stratio.com>
2014-06-10 17:36:51 +0200

    imported test entity classes

--- bin/stratio-deep-init.scala
+++ bin/stratio-deep-init.scala
@@ -6,6 +6,7 @@ import org.apache.cassandra.db.marshal.LongType;
 import com.stratio.deep.config.{DeepJobConfigFactory=>Cfg, _};
 import com.stratio.deep.entity._;
 import com.stratio.deep.context._;
-import com.stratio.deep.rdd._
+import com.stratio.deep.rdd._;
+import com.stratio.deep.testentity._;
 
 val deepContext = new DeepSparkContext(sc)
