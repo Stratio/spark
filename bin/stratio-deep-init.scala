c69b7530b02d983c3064590fa1ae7e5e314379e3
Luca Rosellini <luca@stratio.com>
2014-07-16 13:33:41 +0200

    Revert "Revert "Added com.stratio.deep.rdd.mongodb import""
    This reverts commit 191fe669ab5c2b98e9fe369b36b77cda3224ff96.

--- bin/stratio-deep-init.scala
+++ bin/stratio-deep-init.scala
@@ -7,6 +7,7 @@ import com.stratio.deep.config.{DeepJobConfigFactory=>Cfg, _};
 import com.stratio.deep.entity._;
 import com.stratio.deep.context._;
 import com.stratio.deep.rdd._;
+import com.stratio.deep.rdd.mongodb._;
 import com.stratio.deep.testentity._;
 
 val deepContext = new DeepSparkContext(sc)
