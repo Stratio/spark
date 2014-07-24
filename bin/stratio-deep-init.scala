ed082950fecade3df5f0cf25c961a7be2ce43cc1
rcrespodelosreyes <ricardo.e.crespo@gmail.com>
2014-07-24 12:42:45 +0200

    new imports added

--- bin/stratio-deep-init.scala
+++ bin/stratio-deep-init.scala
@@ -3,11 +3,15 @@ import com.stratio.deep.annotations.DeepField;
 import com.stratio.deep.entity.IDeepType;
 import org.apache.cassandra.db.marshal.Int32Type;
 import org.apache.cassandra.db.marshal.LongType;
-import com.stratio.deep.config.{DeepJobConfigFactory=>Cfg, _};
+import com.stratio.deep.config._
+import com.stratio.deep.config.{CassandraConfigFactory=>Cfg};
+import com.stratio.deep.config.{MongoConfigFactory=>Mcfg_};
 import com.stratio.deep.entity._;
 import com.stratio.deep.context._;
 import com.stratio.deep.rdd._;
 import com.stratio.deep.rdd.mongodb._;
 import com.stratio.deep.testentity._;
+import org.apache.spark.rdd.RDD;
 
-val deepContext = new DeepSparkContext(sc)
+val cassandraDeepContext = new CassandraDeepSparkContext(sc);
+val mongoDeepContext = new MongoDeepSparkContext(sc);
