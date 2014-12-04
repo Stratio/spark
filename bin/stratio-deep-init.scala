551c14871bb0ccec1c9c991fc9b9b710cb04d2f7
rcrespodelosreyes <ricardo.e.crespo@gmail.com>
2014-12-04 12:13:11 +0100

    added aerospike

--- bin/stratio-deep-init.scala
+++ bin/stratio-deep-init.scala
@@ -6,28 +6,23 @@ import com.stratio.deep.commons.entity._
 
 //Core
 import com.stratio.deep.core.context._
-import com.stratio.deep.core.entity._
 
 //Cassandra
 import com.stratio.deep.cassandra.config._
-import com.stratio.deep.cassandra.entity._
 import com.stratio.deep.cassandra.extractor._
-import com.stratio.deep.cassandra.testentity._
+
 
 //MongoDB
 import com.stratio.deep.mongodb.config._
 import com.stratio.deep.mongodb.extractor._
 
-
 //ES
 import com.stratio.deep.es.config._
 import com.stratio.deep.es.extractor._
 
-
-//Apache-Cassandra
-import org.apache.cassandra.db.marshal.Int32Type;
-import org.apache.cassandra.db.marshal.LongType;
-
+//Aerospike
+import com.stratio.deep.aerospike.config._
+import com.stratio.deep.aerospike.extractor._
 
 //Spark
 import org.apache.spark.rdd._
